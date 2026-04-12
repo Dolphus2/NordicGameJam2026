0ca67be1bec489af2a6b990909a2d385fc1224cc
gabriel-laptop-omarchy <gabrielruge@gmail.com>
2026-04-12 11:28:14 +0200

    explosion sound

--- scripts/player_piece.gd
+++ scripts/player_piece.gd
@@ -1,5 +1,11 @@
 extends RigidBody2D
 
+func spawn(points, v):
+	$CollisionPolygon2D.set_deferred("polygon", points)
+	$CollisionPolygon2D/Polygon2D.polygon = points
+	$CollisionPolygon2D/Polygon2D.uv = points
+	linear_velocity = v
+	$AudioStreamPlayer2D.play(1.3)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
