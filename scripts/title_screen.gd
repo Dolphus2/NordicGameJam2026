43ca4b9ca8f7798b881436d732d28512d8f4de8d
amelie tan <amelie.tan@epfl.ch>
2026-04-13 21:22:59 +0200

    fixes

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -3,7 +3,6 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-	print("why")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
